32aacbd6a76080946e7e350703cd915503303f55
Touha Zohair <touha98@gmail.com>
2020-08-07 18:39:43 +0600

    order lifecycle-beforecreate and product controller added

--- api/order/models/order.js
+++ api/order/models/order.js
@@ -1,8 +1,41 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      try {
+        const promises = [];
+        let total = 0;
+        const deliveryOpts = await strapi
+          .query("delivery-charge")
+          .find({ _limit: 1 });
+        const { area, charge } = deliveryOpts[0];
+        data.items.map((item) => {
+          const promise = strapi.query("product").findOne({ id: item.product });
+          promises.push(promise);
+        });
+        const products = await Promise.all(promises);
+        products.forEach((product, index) => {
+          total +=
+            data.items[index].units *
+            (product.price - product.price * (product.discount / 100));
+        });
+        const cost = area.lowAreas.includes(data.address[area.type])
+          ? charge.low
+          : charge.high;
+        data.total = total + cost;
+        data.address.charge = cost;
+        data.confirmed = false;
+        data.paid = 0;
+        data.status = "received";
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+};
--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    ctx.query = {
+      ...ctx.query,
+      status: "published",
+    };
+    if (ctx.query._q) {
+      entities = await strapi.services.product.search(ctx.query);
+    } else {
+      entities = await strapi.services.product.find(ctx.query);
+    }
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.product })
+    );
+  },
+};
