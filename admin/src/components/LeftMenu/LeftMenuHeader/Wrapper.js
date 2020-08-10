ca1ad1055091814e8d5887b5969c0b8caf0a8724
Touha Zohair <touha98@gmail.com>
2020-08-11 03:07:08 +0600

    admin panel logo customization

---
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import Logo from "../../../assets/images/logo.svg";
+
+const Wrapper = styled.div`
+  background-color: #cbad6d;
+  padding-left: 5px;
+  height: ${(props) => props.theme.main.sizes.leftMenu.height};
+
+  .leftMenuHeaderLink {
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  .projectName {
+    display: block;
+    width: 100%;
+    height: ${(props) => props.theme.main.sizes.leftMenu.height};
+    font-size: 2rem;
+    letter-spacing: 0.2rem;
+    color: $white;
+
+    background-image: url(${Logo});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme: {
+    main: {
+      colors: {
+        leftMenu: {},
+      },
+      sizes: {
+        header: {},
+        leftMenu: {},
+      },
+    },
+  },
+};
+
+Wrapper.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default Wrapper;
