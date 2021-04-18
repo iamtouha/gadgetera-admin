ad767a1c9cf68e09d01d75c038bfc1ad8ed9ed2c
Touha Zohair <touha98@gmail.com>
2021-04-18 22:03:39 +0600

    cors problem on heroku fix: try-1

--- config/server.js
+++ config/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("URL", "http://localhost:1338"),
+  url: env("URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
